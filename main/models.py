5925ea99a7819ece6953f9a5f01b6f9f0a2f1679
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2021-04-12 20:50:13 +0300

    added category_detail.html added mixins.py added CategoryManager update views

--- main/models.py
+++ main/models.py
@@ -8,8 +8,12 @@ from django.urls import reverse
 User = get_user_model()
 
 
+def get_models_for_count(*model_names):
+    return [models.Count(model_name) for model_name in model_names]
+
+
 def get_product_url(obj, viewname):
-    ct_model = obj.__class__.meta.model_name
+    ct_model = obj.__class__._meta.model_name
     return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
 
 
@@ -35,12 +39,35 @@ class LatestProducts:
     objects = LatestProductsManager()
 
 
+class CategoryManager(models.Manager):
+
+    CATEGORY_NAME_COUNT_NAME = {
+        'Notebooks': 'notebook__count',
+        'Smartphone': 'smartphone__count',
+    }
+
+    def get_query_set(self):
+        return super().get_queryset()
+
+    def get_categories_for_left_sidebar(self):
+        models = get_models_for_count('notebook','smartphone')
+        qs = list(self.get_query_set().annotate(*models))
+        data = [
+            dict(name=c.name, url=c.get_absolute_url, count=getattr(c, self.CATEGORY_NAME_COUNT_NAME[c.name]))
+            for c in qs
+        ]
+        return data
+
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name='Category name')
     slug = models.SlugField(unique=True)
+    objects = CategoryManager()
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('catefory_detail', kwargs={'slug':self.slug})
 
 
 class Product(models.Model):
@@ -98,12 +125,6 @@ class Smartphone(Product):
 
     def get_absolute_url(self):
         return get_product_url(self, 'product_detail')
-
-    # @property
-    # def sd(self):
-    #     if self.sd:
-    #         return 'Yes'
-    #     return 'No'
 
 
 class CartProduct(models.Model):
