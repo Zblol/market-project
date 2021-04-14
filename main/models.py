014c33651f3235b080f7c3cfed1052a69240c33e
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2021-04-14 21:21:01 +0300

    added CartMixin update cart view

--- main/models.py
+++ main/models.py
@@ -102,6 +102,7 @@ class Notebook(Product):
     video = models.CharField(max_length=255)
     battery_charge_time = models.CharField(max_length=255)
 
+
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
 
@@ -139,12 +140,15 @@ class CartProduct(models.Model):
     def __str__(self):
         return "Product: {} (for cart)".format(self.content_object.title)
 
+    def save(self,*args,**kwargs):
+        self.total_price = self.count * self.content_object.price
+        super().save(*args,**kwargs)
 
 class Cart(models.Model):
-    owner = models.ForeignKey('Customer', verbose_name='Customer', on_delete=models.CASCADE)
+    owner = models.ForeignKey('Customer',null=True, verbose_name='Customer', on_delete=models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='total price')
+    final_price = models.DecimalField(max_digits=9,default=0, decimal_places=2, verbose_name='total price')
     in_order = models.BooleanField(default=False)
     for_anonymous_user = models.BooleanField(default=False)
 
@@ -154,8 +158,8 @@ class Cart(models.Model):
 
 class Customer(models.Model):
     user = models.ForeignKey(User, verbose_name='Customer', on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20, verbose_name='phone number')
-    address = models.CharField(max_length=255, verbose_name='Address')
+    phone = models.CharField(max_length=20, verbose_name='phone number',null=True,blank=True)
+    address = models.CharField(max_length=255, verbose_name='Address',null=True,blank=True)
 
     def __str__(self):
         return "Customer: {} {}".format(self.user.first_name, self.user.last_name)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, View
-from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
-from .mixins import CategoryDetailMixin
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
+from .mixins import CategoryDetailMixin , CartMixin
+from django.http import HttpResponseRedirect
+from django.contrib.contenttypes.models import ContentType
 
 
-class BaseView(View):
+class BaseView(CartMixin,View):
 
     def get(self, request, *args, **kwargs):
         categories = Category.objects.get_categories_for_left_sidebar()
@@ -14,6 +16,7 @@ class BaseView(View):
         context = {
             'categories': categories,
             'products': products,
+            'cart':self.cart,
         }
         return render(request, 'base.html', context)
 
@@ -33,6 +36,11 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
+
 
 class CategoryDetailView(CategoryDetailMixin, DetailView):
     model = Category
@@ -42,14 +50,26 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     slug_url_kwarg = 'slug'
 
 
-class CartView(View):
+class AddToCartView(CartMixin,View):
 
     def get(self, request, *args, **kwargs):
-        customer = Customer.objects.get(user=request.user)
-        cart = Cart.objects.get(owner=customer)
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=self.cart.owner, cart=self.cart, content_type=content_type,object_id=product.id
+        )
+        if created:
+            self.cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
+
+
+class CartView(CartMixin,View):
+
+    def get(self, request, *args, **kwargs):
         categories = Category.objects.get_categories_for_left_sidebar()
         context = {
-            'cart': cart,
+            'cart': self.cart,
             'categories': categories,
         }
         return render(request, 'cart.html', context)
